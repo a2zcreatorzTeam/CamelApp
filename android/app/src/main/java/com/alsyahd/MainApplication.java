ded5d698e6af20184c87015b851b0403d4943898
jotilohana <jklohana208@gmail.com>
2023-10-06 10:50:44 +0500

    fixes

--- android/app/src/main/java/com/alsyahd/MainApplication.java
+++ android/app/src/main/java/com/alsyahd/MainApplication.java
@@ -15,6 +15,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -23,7 +24,10 @@ public class MainApplication extends Application implements ReactApplication {
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
-
+    @Override
+      protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
     @Override
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")

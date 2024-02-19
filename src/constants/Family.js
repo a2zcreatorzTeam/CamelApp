bd431b2944a34e6ca5f2689e31c7c8790bad753a
jotilohana <jklohana208@gmail.com>
2024-02-19 14:15:51 +0500

    fixes

--- src/constants/Family.js
+++ src/constants/Family.js
@@ -1,9 +1,10 @@
 export const family = {
-  Neo_Regular: 'NeoSansArabicRegular',
   Neo_Black: 'NeoSansArabicBlack',
   Neo_Bold: 'NeoSansArabicBold',
+  Neo_Regular: 'NeoSansArabicRegular',
   Neo_Light: 'NeoSansArabicLight',
   Neo_Medium: 'NeoSansArabicMedium',
   Neo: 'NeoSansArabic',
   Neo_Medium2: 'Neo_Sans_Medium',
+  Neo_Regular1: 'Neo Sans Arabic Regular',
 };
